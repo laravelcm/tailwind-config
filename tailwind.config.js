1cc39ac28f917601cd6aa2107df970cabbc2c626
Arthur Monney <monneylobe@gmail.com>
2022-01-07 02:18:20 +0100

    installation et configuration de tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Inter var', 'ui-sans-serif', 'system-ui'],
+      },
+    },
+  },
+  plugins: [],
+}
